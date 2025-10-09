4554f60d46335c6fc975a46c2742b8bc25bad550
christine <diangochristine7@gmail.com>
2025-10-09 11:24:52 +0200

    Ajout du fichier server-entry.js

---
+++ api/src/server-entry.js
@@ -0,0 +1,8 @@
+// api/src/server-entry.js
+import app from './server.js';
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
